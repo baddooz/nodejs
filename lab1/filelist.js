450d589c625780f6f007365689a87c58a242ff3d
bsubram <bsubram@wal-mart.com>
2016-01-04 21:27:19 -0800

    exercise lab 1

---
+++ lab1/filelist.js
@@ -0,0 +1,27 @@
+let fs = require('pn/fs')
+let path = require('path')
+let lodash = require('lodash')
+let {dir} = require('yargs').default('dir', __dirname).argv
+
+async function ls(rootPath){
+   try {
+     	if(fs.statSync(rootPath).isFile()){
+ 			return [rootPath]
+ 		} 
+     	let fileNames = await fs.readdir(rootPath)
+     	let lspromises = []
+     	 for (let fileName of fileNames) {
+     	 	let filePath = path.join(rootPath, fileName)
+		 	 	 	let promise = ls(filePath)
+		    	 	lspromises.push(promise)
+    	}
+    	 return lodash.flatten(await Promise.all(lspromises))
+
+    } catch (e) {
+        console.log(e.stack)
+    }
+}
+
+console.log('executing dir_list function')
+
+ls(dir).then(filePaths => console.log(filePaths))
