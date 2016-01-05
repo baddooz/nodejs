17a29672f132e0203058337422c468f562bf1c8d
bsubram <bsubram@wal-mart.com>
2016-01-05 11:36:26 -0800

    removed callback

--- assignment1/touch.js
+++ assignment1/touch.js
@@ -2,18 +2,16 @@
 
 fs = require('pn/fs')
 
-
 if (process.argv.length < 3) {
     	console.error('Usage: touch.js <file list separated by space>');
     	process.exit(1);
 }
 
 	var filename = process.argv[2];
-	fs.exists(filename,function (exists) {
-	    if (exists) {
+	fs.exists(filename).then( function() {
 	    	var currentTime = new Date();
-	        fs.utimesSync(filename,currentTime,currentTime);
-	    } else {
-	    	fs.open(filename,"w");
-	    }
-	});
+	        fs.utimes(filename,currentTime,currentTime);
+	}
+	).catch(
+	    	fs.open(filename,"w")
+);
