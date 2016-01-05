4e4a819304211652c574d025805a9956cc632a92
bsubram <bsubram@wal-mart.com>
2016-01-05 14:03:25 -0800

    final one

---
+++ assignment1/masterscript.sh
@@ -0,0 +1,45 @@
+./echo.js hello welcome
+
+sleep 2
+
+./cat.js package.json unknown
+
+sleep 4
+
+clear
+
+ls -al
+
+sleep 2
+./touch.js whereismyfile
+
+sleep 2
+ls -al
+
+sleep 1
+
+./touch.js echo.js
+
+sleep4
+
+clear
+
+./ls.js /tmp/ -R
+
+sleep 3
+
+ls -al /tmp/
+
+./mkdir.js /tmp/first/second/third/fourth
+
+ls -al /tmp/first/second/third/fourth/
+
+sleep 4
+
+./rm.js /tmp/first/
+
+sleep 2
+
+ls -al /tmp/first
+
+
