5aba194fffe3475d54347cfa427104f61d0975ee
bsubram <bsubram@wal-mart.com>
2016-01-07 14:29:21 -0800

    assignment 2

---
+++ assignment2/client/client.js
@@ -0,0 +1,94 @@
+let path=require('path')
+let fs = require('pn/fs')
+let http = require('http')
+let request = require('request')
+let mkdirp = require('mkdirp')
+let argv = require('yargs')
+.default('clientdir', '/tmp/VFSClient')
+.argv
+var tuple = require('tuple');
+//var rimraf = require('rimraf')
+
+
+
+let server = '127.0.0.1'
+let nssocket = require('nssocket')
+require('songbird')
+
+
+var outbound = new nssocket.NsSocket();
+outbound.connect(8001,server);
+
+
+console.log("connected to 8001")
+
+outbound.data('filesync',function (data) {
+
+	let jsondata = JSON.parse(data)
+
+	console.log(jsondata)
+	var filePath = jsondata.path 
+	if (jsondata.action === 'delete') {
+		fs.promise.unlink(argv.clientdir+filePath)
+		.then(stat => console.log(argv.clientdir+filePath, ' deleted'))
+	}
+	else if (jsondata.action === 'addDir') {
+		// this is never called
+		console.log('adding dir')
+		let pathWithoutFileName = path.resolve(path.join(argv.clientdir,filePath.substr(0,filePath.lastIndexOf(path.sep))));
+		let localfileName = path.resolve(path.join(pathWithoutFileName,filePath.substr(filePath.lastIndexOf(path.sep)+1)));
+		console.log('dirname = '+pathWithoutFileName +' to be created')
+		let index = pathWithoutFileName.startsWith(path.sep) ? 1:0;
+		fs.access(pathWithoutFileName,fs.F_OK).then(createDirRecursively(pathWithoutFileName,index)).catch(function() {
+			fs.mkdir(dir)
+			return createDir(dirName,index+1);
+		});
+
+	} else {
+		console.log('Downloading the file ', filePath)
+		let pathWithoutFileName = path.resolve(path.join(argv.clientdir,filePath.substr(0,filePath.lastIndexOf(path.sep))));
+		let localfileName = path.resolve(path.join(pathWithoutFileName,filePath.substr(filePath.lastIndexOf(path.sep)+1)));
+
+		console.log("dir name "  + pathWithoutFileName + " fileName = " + localfileName);
+		async()=>{ await createDir(pathWithoutFileName);}
+
+		let options = {
+			url: 'http://127.0.0.1:8000/'+filePath,
+			method:'GET'
+		}
+		request(options,function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+		    console.log(body)  
+		    let stream =  fs.createWriteStream(localfileName)
+		    stream.once('open', function(fd) {
+		    	stream.write(body)
+		    	stream.end()
+		    })
+		} else {
+			console.log("Oopps");
+		}
+	})
+	}
+})
+
+async function createDir(filePath) {
+	await mkdirp.promise(filePath);
+	console.log("created dir")
+
+}
+
+function createDirRecursively(dirName,index) {
+	let dirSep = path.sep;
+	if (dirName && dirName.split(dirSep) && dirName.split(dirSep)[index] ) {
+		var dir='';
+		for (var i=0;i<= index;i++) {
+			dir = dir+dirName.split(dirSep)[i]+dirSep;
+		}
+		fs.access(dir,fs.F_OK).then(createDir(dirName,index+1)).catch( function() {
+			fs.mkdir(dir)
+			return createDir(dirName,index+1);
+		}
+		)
+	} 
+}
+
--- assignment2/main.js
+++ assignment2/main.js
@@ -7,12 +7,19 @@ let nodeify = require('bluebird-nodeify')
 let rimraf = require('rimraf')
 let mime = require('mime-types')
 let mkdirp = require('mkdirp')
-let yargs = require('yargs')
+let argv = require('yargs')
+.default('dir', '/tmp/VFS')
+.argv
+
 require('songbird')
+let chokidar = require('chokidar')
+let nssocket = require('nssocket')
 
 const NODE_ENV = process.env.NODE_ENV
-const PORT = process.env.PORT
-const ROOT_DIR = process.argv.length > 3 ? process.argv[2] : '/tmp/VFS'
+const PORT = process.env.PORT || 8000
+const ROOT_DIR = argv.dir
+const TCP_PORT = 8001
+
 
 let app = express()
 
@@ -21,9 +28,11 @@ if ( NODE_ENV != 'prod') {
 }
 
 app.set('view engine', 'jade');
-app.listen(8000)
-console.log(`LISTENING @ http://127.0.0.1:8000`)
+app.listen(PORT)
+console.log(`LISTENING @ http://127.0.0.1:{PORT}`)
+console.log('My root dir is: ',ROOT_DIR)
 app.head('*', setFileMeta,sendHeaders,(req,res) => res.end());
+
 app.get('*', 
   setFileMeta,
   sendHeaders,
@@ -33,17 +42,25 @@ app.get('*',
       return;
     }
     if(!req.stat) return res.send(400,'Invalid path')
-      fs.createReadStream(req.filePath,res.body);
+      fs.createReadStream(req.filePath).pipe(res);
   });
 
 app.post('*',setFileMeta, setDirDetails,(req,res,next)=> {
   async()=> {
-    if(!req.stat) return res.send(405,'File does not exist')
-      if(req.isDir) return res.send(405,'Path is a directory')
-        await fs.promise.truncate(req.filePath,0)
-      req.pipe(fs.createWriteStream(req.filePath))
-      res.end()
+    if(!req.stat) {
+      res.send(405,'File does not exist');
+      return;
+    } 
+    if(req.isDir) { 
+      res.send(405,'Path is a directory')
+      return;
     }
+      await fs.promise.truncate(req.filePath,0)
+      console.log('dir created');
+      req.pipe(fs.createWriteStream(req.filePath))
+      console.log('file is  updated');
+      res.end()
+    }().catch();
   });
 app.put('*',setFileMeta, setDirDetails,(req,res,next)=> { 
   async()=> {
@@ -71,6 +88,48 @@ app.delete('*',setFileMeta, (req,res,next)=> {
     }().catch(next)
   });
 
+
+// listening for clients
+var sockets = [];
+var server = nssocket.createServer(
+  function (socket) {
+    sockets.push(socket);
+    console.log("total no of socktes to broadcast = " + sockets.length);
+    socket.data('Connecting', function (data) {
+      for(var i=0, l=sockets.length; i<l; i++) {
+        sockets[i].send('Broadcasting', 'data');
+      }
+    });
+  }
+  ).listen(TCP_PORT);
+
+console.log(`LISTENING ${TCP_PORT} for Dropbox Clients`)
+
+let watcher = chokidar.watch(ROOT_DIR, {ignored: /[\/\\]\./,ignoreInitial: true})
+
+
+// Add event listeners. 
+watcher
+.on('add', path => broadCastData('write', false, path))
+.on('addDir',path => broadCastData('addDir', false, path))
+.on('change', path => broadCastData('write', false, path))
+.on('unlink', path => broadCastData('delete', false, path));
+
+function broadCastData(action, dir, path)
+{
+  let strippedpath = path.substr(ROOT_DIR.length);
+  console.log("stripped path = ",strippedpath);
+  let jsonString = '{ "action": "' + action + '","path":'
+  jsonString = jsonString +  '"' + strippedpath + '","type": "'
+  jsonString = jsonString + dir + '"}'
+
+  for(var i=0, l=sockets.length; i<l; i++) {
+    sockets[i].send('filesync', jsonString);
+
+  }
+  console.log(" sent ")
+}
+
 function setFileMeta(req,res,next) {
 
   req.filePath = path.resolve(path.join(ROOT_DIR,req.url))
@@ -85,31 +144,30 @@ function setFileMeta(req,res,next) {
 
 function setDirDetails(req,res,next) {
 
-  if (req.stat) return res.send(405,'File exists')
-    let filePath = req.filePath
+  //if (!req.stat) return res.send(405,'File exists')
+  let filePath = req.filePath
   let endsWith = filePath.charAt(filePath.length-1) === path.sep
   let hasExt = path.extname(req.filePath) !== ''
   req.isDir = endsWith  || !hasExt
   req.dirPath = req.isDir ? filePath : path.dirname(filePath);
-  console.log("filePath = "+ filePath + " length = "+filePath.length+" char at = "+filePath.charAt(filePath.length-1))
-
-
+ // console.log("filePath = "+ filePath + " length = "+filePath.length+" char at = "+filePath.charAt(filePath.length-1))
   next()
 }
 
 function sendHeaders(req,res,next) {
   nodeify( async ()=> {
-    console.log("sendHeaders req.stat = ",req.stat)
+    //console.log("sendHeaders req.stat = ",req.stat)
     if (req.stat && req.stat.isDirectory()) {
+      //console.log("its a dir")
       let files = await fs.promise.readdir(req.filePath)
       res.body = JSON.stringify(files)
       res.setHeader('Content-Length',res.body.length)
-      //let contentType = mime.content
       res.setHeader('Content-Type', 'application/json')
       return
     }
     if (req.stat) res.setHeader('Content-Length',req.stat.size)
-      let contentType = mime.contentType(path.extname(req.filePath))
+    let contentType = mime.contentType(path.extname(req.filePath))
     res.setHeader('Content-Type',contentType)
+    console.log("done sendHeaders");
   }(),next)
 }
